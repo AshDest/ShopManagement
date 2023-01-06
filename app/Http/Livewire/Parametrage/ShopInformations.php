d784d0766a1d3901ab521017dab889794df32bf4
AshDest <ashuzamaheshe4@gmail.com>
2023-01-06 08:25:09 +0200

    ash commit

---
+++ app/Http/Livewire/Parametrage/ShopInformations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Parametrage;
+
+use Livewire\Component;
+
+class ShopInformations extends Component
+{
+    public function render()
+    {
+        return view('livewire.parametrage.shop-informations');
+    }
+}
