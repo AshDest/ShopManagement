9331ae55126191a13d6af72c89b314a3d60b03e3
AshDest <ashuzamaheshe4@gmail.com>
2023-01-16 09:53:51 +0100

    ash commit

---
+++ app/Http/Livewire/Terminal/Paiements.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Terminal;
+
+use Livewire\Component;
+
+class Paiements extends Component
+{
+    public function render()
+    {
+        return view('livewire.terminal.paiements');
+    }
+}
