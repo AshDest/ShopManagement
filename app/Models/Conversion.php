a86669bee1869e3fdc20ccfbe32ab60f860d1157
AshDest <ashuzamaheshe4@gmail.com>
2023-01-17 14:39:37 +0100

    ash commit

--- app/Models/Conversion.php
+++ app/Models/Conversion.php
@@ -10,11 +10,18 @@ class Conversion extends Model
     use HasFactory;
     protected $fillable = [
         'produit_id',
+        'quantite',
+        'produit_code',
+        'qte_ajout',
         'motif',
     ];
 
     public function conversion()
     {
-        return $this->belongsTo(Project::class, 'produit_id');
+        return $this->belongsTo(Produit::class, 'produit_id');
+    }
+    public function convertis()
+    {
+        return $this->belongsTo(Produit::class, 'produit_code');
     }
 }
--- database/migrations/2023_01_03_140831_create_conversions_table.php
+++ database/migrations/2023_01_03_140831_create_conversions_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('produit_id');
             $table->foreign('produit_id')->references('id')->on('produits')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('quantite')->default('0');
+            $table->unsignedBigInteger('produit_code');
+            $table->foreign('produit_code')->references('id')->on('produits')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('qte_ajout')->default('0');
             $table->text('motif')->nullable();
             $table->timestamps();
         });
