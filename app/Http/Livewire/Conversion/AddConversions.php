b98ef0dca5aa673462c7c0834bc9f4ff7c35aa60
AshDest <ashuzamaheshe4@gmail.com>
2023-01-21 14:29:06 +0100

    ash commit

--- app/Http/Livewire/Conversion/AddConversions.php
+++ app/Http/Livewire/Conversion/AddConversions.php
@@ -6,10 +6,14 @@ use App\Models\Conversion;
 use App\Models\Produit;
 use Livewire\Component;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\WithPagination;
 
 class AddConversions extends Component
 {
+    use WithPagination;
     use LivewireAlert;
+    public  $reseach, $page_active = 4;
+
     public $selectProdui1;
     public $quantite;
     public $selectProdui2;
@@ -19,7 +23,6 @@ class AddConversions extends Component
     public $oldqte;
     public $oldquantite;
 
-    public $produits;
     public $unite;
     public $unite2;
 
@@ -62,12 +65,25 @@ class AddConversions extends Component
         $vars = Produit::whereId($id)->first();
         $this->unite2 = $vars->designationmesure;
     }
-    public function mount()
-    {
-        $this->produits = Produit::all();
-    }
+    // public function mount()
+    // {
+    //     $this->produits = Produit::all();
+    // }
     public function render()
     {
-        return view('livewire.conversion.add-conversions');
+        // return view('livewire.conversion.add-conversions');
+        if ($this->reseach) {
+            return view('livewire.conversion.add-conversions', [
+                'produits' => Produit::where('description', 'LIKE', '%' . $this->reseach . '%')
+                    ->orwhereHas('categorie', function ($s) {
+                        $s->where('designation', 'LIKE', '%' . $this->reseach . '%');
+                    })
+                    ->paginate($this->page_active)
+            ]);
+        } else {
+            return view('livewire.conversion.add-conversions', [
+                'produits' => Produit::orderBy('id', 'DESC')->paginate($this->page_active)
+            ]);
+        }
     }
 }
--- resources/views/livewire/conversion/add-conversions.blade.php
+++ resources/views/livewire/conversion/add-conversions.blade.php
@@ -7,20 +7,74 @@
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="mb-3">
-                                    <label for="simpleinput" class="form-label">Produit à Convertir</label>
-                                    <select name="selectProdui1" id="code" wire:model='selectProdui1'
-                                        class="form-select">
-                                        <option value=""></option>
-                                        @foreach ($produits as $produit)
-                                        <option value="{{$produit->id}}">{{$produit->description}}</option>
-                                        @endforeach
-                                    </select>
-                                    <div class="valid-feedback">
-                                        @error('selectProdui1')
-                                        <span style="color: red;">{{ $message }}</span>
-                                        @enderror
-                                    </div>
+                                    <form
+                                        class="row gy-2 gx-2 align-items-center justify-content-xl-start justify-content-between">
+                                        <div class="col-auto">
+                                            <label for="inputPassword2" class="visually-hidden">Search</label>
+                                            <input type="search" wire:model='reseach' class="form-control"
+                                                id="inputPassword2" placeholder="rechercher le produit...">
+                                        </div>
+                                    </form>
                                 </div>
+                                <div class="table-responsive">
+                                    <table class="table table-centered table-nowrap table-hover mb-0">
+                                        <tbody>
+                                            @forelse ($produits as $produit)
+                                            <tr>
+                                                <td>
+                                                    <h5 class="font-14 my-1"><a href="javascript:void(0);"
+                                                            class="text-body">{{$produit->description}}</a></h5>
+                                                    <span
+                                                        class="text-muted font-13">{{$produit->categorie->designation}}</span>
+                                                </td>
+                                                <td>
+                                                    <span
+                                                        class="text-muted font-13">{{$produit->designationmesure}}</span>
+                                                    <br />
+                                                    @if ($produit->qte_stock > 10)
+                                                    <span
+                                                        class="badge badge-success-lighten">{{$produit->qte_stock}}</span>
+                                                    @elseif ($produit->qte_stock < 10 && $produit->qte_stock > 4)
+                                                        <span
+                                                            class="badge badge-warning-lighten">{{$produit->qte_stock}}</span>
+                                                        @else
+                                                        <span
+                                                            class="badge badge-danger-lighten">{{$produit->qte_stock}}</span>
+                                                        @endif
+                                                </td>
+                                                <td>
+                                                    <span class="text-primary fw-semibold">PA : @php
+                                                        echo number_format($produit->pu_achat). ' CDF';
+                                                        @endphp</span>
+                                                    <h5 class="text-success fw-semibold">PV : @php
+                                                        echo number_format($produit->pu). ' CDF';
+                                                        @endphp</h5>
+                                                </td>
+                                                <td>
+                                                    <span class="text-muted font-13">Variation de Stock</span>
+                                                    <h5 class="font-14 mt-1 fw-normal">{{$produit->updated_at}}</h5>
+                                                </td>
+                                                @if ($produit->qte_stock > 1)
+                                                <td class="text-end">
+                                                    <a href="javascript:void(0);" class="font-18 text-info me-2"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top"
+                                                        title="Selectionner le Produit"><i
+                                                            class="uil uil-focus-add"></i></a>
+                                                </td>
+                                                @endif
+                                            </tr>
+                                            @empty
+
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                    <br>
+                                    <center>
+                                        @if (count($produits))
+                                        {{ $produits->links('vendor.livewire.bootstrap') }}
+                                        @endif
+                                    </center>
+                                </div> <!-- end table-responsive-->
                             </div>
                             <div class="col-lg-6">
                                 <label class="form-label">Quantité /{{$unite}}</label>
@@ -59,8 +113,8 @@
                             </div>
                             <button class="btn btn-primary" type="submit">Enregistrer la Conversion</button>
                         </div>
-                    </form>
 
+                    </form>
                 </div> <!-- end card-body-->
             </div> <!-- end card-->
         </div> <!-- end col -->
