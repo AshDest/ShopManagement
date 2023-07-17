4bfc1176d30f68b6968fe291a0437dc0ec539268
gracieuxsikuly <graciersikuly@gmail.com>
2023-07-17 16:40:53 +0200

    okays

--- database/migrations/2023_01_03_140831_create_conversions_table.php
+++ database/migrations/2023_01_03_140831_create_conversions_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('conversions', function (Blueprint $table) {
             $table->id();
-            $table->integer('produit_id')->unsigned()->index();;
-            $table->foreign('produit_id', 'fk_conversions_produit_id')->references('id')->on('produits')->onDelete('cascade')->onUpdate('cascade');
-            $table->integer('quantite')->default('0');
+            $table->integer('produit_id')->unsigned()->index();
             $table->integer('produit_code')->unsigned()->index();
-            $table->foreign('produit_code', 'fk_conversions_produit_code')->references('produit_code')->on('produits')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('qte_ajout')->default('0');
+            $table->integer('quantite')->default('0');
             $table->text('motif')->nullable();
+            $table->foreign('produit_id', 'fk_conversions_produit_id')->references('id')->on('produits')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('produit_code', 'fk_conversions_produit_code')->references('produit_code')->on('produits')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
 
