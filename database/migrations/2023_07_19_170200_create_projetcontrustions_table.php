3d4d685a7f5084db82027651f643157a9f548025
gracieuxsikuly <graciersikuly@gmail.com>
2023-07-21 22:43:14 +0200

    migration projet edit

--- database/migrations/2023_07_19_170200_create_projetcontrustions_table.php
+++ database/migrations/2023_07_19_170200_create_projetcontrustions_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('designationprojet');
             $table->string('responsableprojet');
             $table->string('contactreponsable');
-            $table->enum('statutprojet',['encours','pending','cloture'])->default('encours');
+            $table->enum('statutprojet',['encours','pending','cloturer'])->default('encours');
             $table->timestamps();
         });
     }
