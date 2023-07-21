be67398139a0090ce523c306f43ccac232967a9c
gracieuxsikuly <graciersikuly@gmail.com>
2023-07-22 00:11:30 +0200

    go go

--- resources/views/livewire/construction/details-depense-wire.blade.php
+++ resources/views/livewire/construction/details-depense-wire.blade.php
@@ -1,4 +1,111 @@
 <div>
+    <div class="row">
+        <div class="col-xxl-8 col-lg-8">
+            <!-- project card -->
+            <div class="card d-block">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-2">
+                        <h3 class="">App design and development {{ $this->idprojet }}</h3>
+                        <div class="dropdown">
+                            <a href="#" class="dropdown-toggle arrow-none card-drop"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="dripicons-dots-3"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <!-- item-->
+                                <a style="cursor: pointer;"
+                                    wire:click="changerstatus({{ $this->idprojet }},'Cloturer')"
+                                    class="dropdown-item"><i
+                                        class="mdi mdi-progress-close"></i>&ensp;Cloturer</a>
+                                <!-- item-->
+                                <a style="cursor: pointer;"
+                                    wire:click="editdepense({{ $this->idprojet }},'Pending')"
+                                    class="dropdown-item"><i
+                                        class="mdi mdi-progress-alert"></i>&ensp;En attente</a>
+                                <!-- item-->
+                                <a style="cursor: pointer;"
+                                    wire:click="editdepense({{ $this->idprojet }},'Encours')"
+                                    class="dropdown-item"><i
+                                        class="mdi mdi-progress-clock"></i>&ensp;Encours</a>
+                            </div>
+                        </div>
+                        <!-- project title-->
+                    </div>
+                    @if ($this->statut_projet == 'Encours')
+                        <span class="badge bg-primary-lighten text-primary">{{$this->statut_projet}}</span>
+                    @elseif ($this->statut_projet == 'Pending')
+                        <span class="badge bg-warning-lighten text-warning">{{$this->statut_projet}}</span>
+                    @else
+                        <span class="badge bg-success-lighten text-success">{{$this->statut_projet}}</span>
+                    @endif
+
+                    <h5>Project Overview:</h5>
+
+                    <p class="text-muted mb-2">
+                        With supporting text below as a natural lead-in to additional contenposuere erat
+                        a ante. Voluptates, illo, iste itaque voluptas
+                        corrupti ratione reprehenderit magni similique? Tempore, quos delectus
+                        asperiores libero voluptas quod perferendis! Voluptate,
+                        quod illo rerum? Lorem ipsum dolor sit amet.
+                    </p>
+
+                    <p class="text-muted mb-4">
+                        Voluptates, illo, iste itaque voluptas corrupti ratione reprehenderit magni
+                        similique? Tempore, quos delectus asperiores
+                        libero voluptas quod perferendis! Voluptate, quod illo rerum? Lorem ipsum dolor
+                        sit amet. With supporting text below
+                        as a natural lead-in to additional contenposuere erat a ante.
+                    </p>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="mb-4">
+                                <h5>Start Date</h5>
+                                <p>17 March 2018 <small class="text-muted">1:00 PM</small></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-4">
+                                <h5>End Date</h5>
+                                <p>22 December 2018 <small class="text-muted">1:00 PM</small></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-4">
+                                <h5>Budget</h5>
+                                <p>$15,800</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div> <!-- end card-body-->
+
+            </div> <!-- end card-->
+
+
+        </div> <!-- end col -->
+
+        <div class="col-lg-4 col-xxl-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title mb-3">Progress</h5>
+                    <div dir="ltr">
+                        <div class="mt-3 chartjs-chart" style="height: 320px;">
+                            {{-- <div dir="ltr"> --}}
+                            <div id="chartdepense" class="apex-charts" data-colors="#727cf5,#e3eaef">
+                            </div>
+                            {{-- </div> --}}
+                            {{-- <canvas id="line-chart-example"></canvas> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end card-->
+
+
+        </div>
+    </div>
     {{-- Success is as dangerous as failure. --}}
 
     @push('jschart_depense')
